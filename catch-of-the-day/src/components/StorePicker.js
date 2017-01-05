5384a8cb8d97f2d675051974e4628ed90ef394f0
Stacey Fenton <s.fenton@ry.com>
2017-01-05 17:54:10 +0000

    first component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form className="store-selector">
+        { /* JSX comment style */ }
+        <h2>Please enter a store</h2>
+        <input type="text" required placeholder="Store name" />
+        <button type="submit">Visit store</button>
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
