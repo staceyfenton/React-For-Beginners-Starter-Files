74fac32dba3988158caba529ae269bb2550f47d3
Stacey Fenton <staceyfenton@PEG-MAC-008149.local>
2018-04-24 16:13:29 +0100

    Reset solution to latest version

--- catch-of-the-day/src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-  apiKey: "AIzaSyD-1OCNGfA39orieFTK8QCXVI9IIUNPG2Y",
-  authDomain: "catch-of-the-day-stacey-fenton.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-stacey-fenton.firebaseio.com",
-});
-
-export default base;
