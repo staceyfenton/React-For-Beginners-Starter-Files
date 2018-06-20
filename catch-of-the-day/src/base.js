26f6de4ee4122707a9fb2e47c395ffa5179f95da
Stacey Fenton <staceyfenton@gmail.com>
2018-06-20 17:27:05 +0100

    remove data

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyD-1OCNGfA39orieFTK8QCXVI9IIUNPG2Y",
-    authDomain: "catch-of-the-day-stacey-fenton.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-stacey-fenton.firebaseio.com"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: ""
 });
 
 const base = Rebase.createClass(firebaseApp.database());
